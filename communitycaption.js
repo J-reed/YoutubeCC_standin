e5b5243225aa692025a5f574bdc5d981924563ec
J-reed <46486685+J-reed@users.noreply.github.com>
2020-10-31 11:12:30 +0000

    Very basic system working
    You can paste caption code in and it will work. You can change the position and size of the captions. You can change the background and the foreground colour

--- communitycaption.js
+++ communitycaption.js
@@ -1,51 +1,86 @@
-function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-}
+caption_text_span_ID = "TEXTAREA";
+
+init_captions();
+var captions_string = {}
+
+var htmlVideoPlayer = document.getElementsByTagName('video')[0];
+htmlVideoPlayer.ontimeupdate = function () {myfunction(captions_string)};
+
 
 function init_captions(){
-    c = document.createElement("DIV");
-    c.innerHTML = "<div class=\"caption-window ytp-caption-window-bottom ytp-caption-window-rollup\" id=\"caption-window-1\" dir=\"ltr\" tabindex=\"0\" aria-live=\"assertive\" style=\"touch-action: none; text-align: left; overflow: hidden; left: 21.2%; width: 517px; height: 46px; bottom: 2%;\" data-layer=\"4\" lang=\"en\"><span class=\"caption-visual-line\" style=\"display: block;\"><span id=\"TEXTDATA\" style=\"display: inline-block; white-space: pre-wrap; background: rgba(8, 8, 8, 0.75) none repeat scroll 0% 0%; font-size: 32px; color: rgb(255, 255, 255); fill: rgb(255, 255, 255); font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;\" class=\"ytp-caption-segment\"></span></span></div>";
     
-     document.getElementById("movie_player").appendChild(c);
+    c = document.createElement("DIV");
+    c.innerHTML = "<div class=\"caption-window ytp-caption-window-bottom ytp-caption-window-rollup\" id=\"caption-window-1\" dir=\"ltr\" tabindex=\"0\" aria-live=\"assertive\" style=\"touch-action: none; text-align: left; overflow: visible; left: 21.2%; width: 517px; height: 46px; bottom: 2%;\" data-layer=\"4\" lang=\"en\"><span class=\"caption-visual-line\" style=\"display: block;\"><span id=\""+caption_text_span_ID+"\" style=\"display: inline-block; white-space: pre-wrap; background: rgba(8, 8, 8, 0.75) none repeat scroll 0% 0%; font-size: 32px; color: rgb(255, 255, 255); fill: rgb(255, 255, 0); font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;\" class=\"ytp-caption-segment\"></span></span></div>";
+    
+    document.getElementById("movie_player").appendChild(c);
+     
 }
 
+
 function getCaptions(){
-    temp = [
-        {
-            "id":"1",
-            "stime":"40",
-            "etime":"50", 
-            "caption":"Hello",
-            "flag": "0"
-        },
-        {
-            "id":"2",
-            "stime":"50",
-            "etime":"60", 
-            "caption":"Bye",
-            "flag": "0"
-        }
-    ];
+ 
+    var enterbox = document.getElementById("caption_content");
+    captions_string = JSON.parse(enterbox.value);
     
-    return temp;
+//    ```[
+//        { 
+//            "stime":"40",
+//            "etime":"50", 
+//            "caption":"Hello",
+//            "flag": "0"
+//        },
+//        {
+//            "stime":"50",
+//            "etime":"60", 
+//            "caption":"Bye",
+//            "flag": "0"
+//        }
+//    ]```;
+    
+}
+
+function updateCaptionSettings(){
+    
+    caption = document.getElementById(caption_text_span_ID);
+    
+    caption.style.background = document.getElementById("background").value;
+    caption.style.fontSize = (document.getElementById("fontsize").value)+"pt";
+    caption.style.color = document.getElementById("colour").value;
+    
+    x_y_width_height = document.getElementById("xywh").value;
+    var list_of_xywh = x_y_width_height.split(",");
+    
+    cap_parent = caption.parentElement.parentElement;
+    cap_parent.style.left = list_of_xywh[0]+"%";
+    cap_parent.style.bottom = list_of_xywh[1]+"%";
+    cap_parent.style.width = list_of_xywh[2]+"px";
+    cap_parent.style.height = list_of_xywh[3]+"px";
+    
+//    style=\"background: rgba(8, 8, 8, 0.75) none repeat scroll 0% 0%; font-size: 32px; color: rgb(255, 255, 255); fill: rgb(255, 255, 255); font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;\"
+    
 }
 
 function set_caption(text){
-    c =document.getElementById("TEXTDATA");
+    c =document.getElementById(caption_text_span_ID);
     c.innerHTML = text;
 }
 
 
-
-sleep(5);
-init_captions();
-
-my_captions = getCaptions();
-
-var htmlVideoPlayer = document.getElementsByTagName('video')[0];
-htmlVideoPlayer.ontimeupdate = function () {myfunction(my_captions)};
-
 function myfunction(data){
+    
+    if(document.getElementById("caption_content")==null){
+        captionenterbox = document.createElement("a");
+        captionenterbox.innerHTML =   "<div style=\"position:inline-block;top:50%; height=-200%;\"><label style=\"font-size\: 14pt\">Paste Caption Code Here</label> <br><textarea id=\"caption_content\" name=\"caption_content\" rows=\"4\" cols=\"50\"></textarea> <br> <br> <label style=\"font-size\: 14pt\">Font Size</label> <br><input id=\"fontsize\" placeholder=\"fontsize\" value=20> <br> <br> <label style=\"font-size\: 14pt\">X, Y, Width, Height</label> <br><input id=\"xywh\" placeholder=\"Position and Size\" value=\"21.2,2,517,46\"><br> <br><label style=\"font-size\: 14pt\">Background Colour</label> <br> <input type=\"color\" id=\"background\" value=\"#000000\"> <br> <br> <label style=\"font-size\: 14pt\">Font Colour</label> <br> <input type=\"color\" id=\"colour\"  value=\"#FFFFFF\"> <br> <br> <input id=captureCaptions type=\"button\" value=\"Apply Captions\"> <input id=captionSettingsUpdate type=\"button\" value=\"Update Caption Settings\"> <br> <br> <br> <br> <br> <br> <br></div>"
+
+        document.getElementById("meta").appendChild(captionenterbox);
+          
+        var captureCaptionsBtn = document.getElementById("captureCaptions")
+        captureCaptionsBtn.addEventListener("click", function(){ my_captions = getCaptions()});
+        
+        var captionsettingsBtn = document.getElementById("captionSettingsUpdate")
+        captionsettingsBtn.addEventListener("click", function(){ updateCaptionSettings()});
+    }
+    
     
     current_time = parseFloat(htmlVideoPlayer.currentTime)
     
@@ -60,6 +95,18 @@ function myfunction(data){
                 data[i].flag = "0"
             }
         }
+    }
+    
+    flag = true;
+    for(var i = 0; i < data.length; i++){
+        if(data[i].flag == "1"){
+            flag = false;
+            break;
+        }
+    }
+    
+    if(flag){
+        set_caption("");
     }
 }
   
